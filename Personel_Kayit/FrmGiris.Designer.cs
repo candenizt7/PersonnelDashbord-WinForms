49684995195199348ecd6fef33859bd522112edf
candenizt7 <cande.topal61@gmail.com>
2025-07-07 22:27:19 +0300

    PersonnelDashbord-WinForms
    C# project for managing personnel records with built-in analytics and visual reports.

---
+++ Personel_Kayit/FrmGiris.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Personel_Kayit
+{
+    partial class FrmGiris
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtKullaniciAd = new System.Windows.Forms.TextBox();
+            this.TxtSifre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGirisYap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kullanıcı Adı:";
+            // 
+            // txtKullaniciAd
+            // 
+            this.txtKullaniciAd.Location = new System.Drawing.Point(199, 105);
+            this.txtKullaniciAd.Name = "txtKullaniciAd";
+            this.txtKullaniciAd.Size = new System.Drawing.Size(153, 31);
+            this.txtKullaniciAd.TabIndex = 1;
+            // 
+            // TxtSifre
+            // 
+            this.TxtSifre.Location = new System.Drawing.Point(199, 151);
+            this.TxtSifre.Name = "TxtSifre";
+            this.TxtSifre.Size = new System.Drawing.Size(153, 31);
+            this.TxtSifre.TabIndex = 3;
+            this.TxtSifre.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(110, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Şifre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(80, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(272, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Personel Takip Sistemi Giriş Paneli";
+            // 
+            // btnGirisYap
+            // 
+            this.btnGirisYap.Location = new System.Drawing.Point(199, 201);
+            this.btnGirisYap.Name = "btnGirisYap";
+            this.btnGirisYap.Size = new System.Drawing.Size(153, 34);
+            this.btnGirisYap.TabIndex = 5;
+            this.btnGirisYap.Text = "Giriş Yap";
+            this.btnGirisYap.UseVisualStyleBackColor = true;
+            this.btnGirisYap.Click += new System.EventHandler(this.btnGirisYap_Click);
+            // 
+            // FrmGiris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 269);
+            this.Controls.Add(this.btnGirisYap);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtSifre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtKullaniciAd);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Corbel", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "FrmGiris";
+            this.Text = "FrmGiris";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtKullaniciAd;
+        private System.Windows.Forms.TextBox TxtSifre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGirisYap;
+    }
+}
